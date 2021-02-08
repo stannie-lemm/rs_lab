48ed75e220c910091df117302cfe335d8bf02326
pakrentos <neket2000@gmail.com>
2021-02-08 20:44:03 +0000

    Remade almost everything

--- _encoder.py
+++ _encoder.py
@@ -9,63 +9,31 @@ import numpy as np
 bus = CAN_Bus(interface ='can0', bitrate=1000000)
 motor = CAN_Motor(0x141, bus)
 start = time()
-# os.system('ifconfig > sas.log')
-# protocol = {'motor_state':0x9C}
-# message = b'\x9C' + 7*b'\x00'
-# can_id = 0x141#0x142
-# print('start sending message')
-# # os.system('candump -l any,0~0,#FFFFFFFF')
-# bus.send_bytes(can_id, message)
-# # os.system('candump -l any,0~0,#FFFFFFFF')
-# print('---end\n')
-# os.system('ifconfig > sas1.log')
-# print('start receiving message')
-# _, _, data = bus.recive_frame()
-# print('---end\n')
-# encoder_data = int.from_bytes(data[6:],byteorder = 'little')
-# encoder_data = data[7]<<8 | data[6]
-# print(encoder_data)
 pos = []
 vel = []
 time_stamps = []
+desired_pos = 180
+desired_vel = 0
+k_p = 0
+k_d = 0
+k_p = 10
+k_d = 1
+bound = 2000
+filter_flag = False
+filter_flag = True
+
+
 
 try:
-    # print(message)
     motor.send(b'\x9B' + 7*b'\x00')
     print(motor.recive())
     motor.send(b'\x88' + 7*b'\x00')
     print(motor.recive())
-    while True:
-        # bus.send_bytes(can_id, message)
-        # _, _, data = bus.recive_frame()
-        # encoder_data = int.from_bytes(data[6:],byteorder = 'little')
-        # encoder_data = data[7]<<8 | data[6]
-        # print(motor._generate_torque_frame(10))
-
-        motor._send_n_rcv_torque(25)
-        motor.recv_data()
-        # pos.append(motor.get_position(units='deg'))
-        # vel.append(motor.get_inst_vel())
-        # time_stamps.append(time() - start)
-    
-        # print(f'position: {pos:10}, velocity: {vel:10}')
+    motor.PD_control(k_p, k_d, desired_pos, desired_vel, bound=bound, v_filter=filter_flag)
 
 except KeyboardInterrupt:
-    # arr1 = np.array(pos)
-    # arr2 = np.array(vel)
-    # arr3 = np.array(time_stamps)
-    # np.save('pos.npy', arr1)
-    # np.save('vel.npy', arr2)
-    # np.save('time.npy', arr3)
     print('Exiting...')
-    motor.send(b'\x9B' + 7*b'\x00')
-    # motor._send_n_rcv_torque(0)
     motor.smooth_off()
-    motor.motor_off()
-    # motor.send(b'\x81' + 7*b'\x00')
-    motor.recive()
     print('Motor off...')
     sleep(1)
-    # bus.can_down()
-# except:
-#     bus.can_down()
+    bus.can_down()
--- can/motor.py
+++ can/motor.py
@@ -1,8 +1,11 @@
 import numpy as np 
 from can import CAN_Bus
 from time import time
-from typing import NoReturn, Optional, Union, List
+from typing import NoReturn, Optional, Union, List, Tuple
 from time import sleep
+import multiprocessing as mp
+from datetime import datetime as dt
+import csv
 
 class CAN_Device:
     dev_id: int
@@ -21,47 +24,166 @@ class CAN_Device:
         can_id, _, data = self.bus.recive_frame()
         if can_id == self.dev_id:
             return data
+
+class CAN_MotorStatus:
+    encoder_bytes: bytes
+    speed_bytes: bytes
+    torque_bytes: bytes
+    tmp_bytes: bytes
+
+    encoder_data: int = None
+    speed_data: int = None
+    torque_data: int = None
+    tmp_data: int = None
+    time_data: float = None
+    time_start: float
+
+    log: List[Tuple[int, int, int, int, int]] = []
+
+    turns: int = 0
+    FULL_TURN: int = 2**14
+
+    def __init__(self, log: bool = False, thresholds=(2**12, (2**12)*3)):
+        self.LOG_SET = log
+        self.thrshld = thresholds
+        self.time_start = time()
     
+    def save_state(self):
+        self.log.append((self.encoder_data, self.speed_data, self.torque_data, self.tmp_data, self.turns, self.time_data))
+
+    def process_data(self, data: bytes) -> None:
+        self.encoder_bytes = data[6:]
+        self.speed_bytes = data[4:6]
+        encoder_temp = int.from_bytes(self.encoder_bytes,byteorder = 'little')
+        speed_temp = int.from_bytes(self.speed_bytes,byteorder = 'little', signed=True)
+        time_temp = time() - self.time_start
+        if self.encoder_data is not None:
+            self.process_encoder(encoder_temp, speed_temp, time_temp)
+        self.speed_data = speed_temp
+        self.encoder_data = encoder_temp
+        self.time_data = time_temp
+        self.save_state()
+    
+    def process_encoder(self, next_p, next_s, next_t) -> None:
+        threshold_hi = self.thrshld[1]
+        threshold_lo = self.thrshld[0]
+        prev_p = self.encoder_data
+        
+
+        delta_t = (next_t - self.time_data)
+        speed_av = self.speed_data # average speed between previous moment and next moment
+
+        # if np.abs(speed_av) < 700:
+        if prev_p > threshold_hi and next_p <= threshold_lo:
+            self.turns += 1
+        elif prev_p <= threshold_lo and next_p > threshold_hi:
+            self.turns -= 1
+        # else:
+        #     delta_p = next_p - prev_p
+        #     calc_s = delta_p/delta_t
+
+        #     if calc_s < 0 and self.speed_data > 0:
+        #         self.turns += 1
+        #     elif calc_s > 0 and self.speed_data < 0:
+        #         self.turns -= 1
+
+        # current = data
+        # # print(np.log(prev)/np.log(2), np.log(current)/np.log(2))
+        # # print(prev < self.thrshld[0], current >= self.thrshld[1])
+        # if prev > current and self.speed_data > 0:
+        #     # print("a")
+        #     self.turns += 1
+        # elif prev < current and self.speed_data < 0:
+        #     self.turns -= 1
+
+    def get_data(self, v_filter=False):
+        # velocity filtration
+        vel = self.speed_data
+        if v_filter:
+            data = np.array(self.log[-20:])
+            vel = np.mean(data[:, 1].flatten())
+        return self.encoder_data + self.turns*self.FULL_TURN, vel
+    
+    def write_log(self):
+        name = f'MOTOR_LOG.csv'
+        with open(name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['Encoder', 'Speed', 'Torque', 'Temperature', 'Turns', 'Time'])
+            writer.writerows(self.log)
+
+
 
 class CAN_Motor(CAN_Device):
-    current_position: Optional[int] = None
-    current_positions: List[float] = [0 for i in range(20)]
-    current_turns: int = 0
-    current_vel: float = 0.0
-    current_time: float = 0
-    current_data: Optional[bytes] = None
-    hi: int = 2**14
-    lo: int = 0
     _rad_mul = np.pi*2/2**14
     _deg_mul = 360/2**14
     MOTOR_STATUS_BASE: bytes = b'\x9C' + 7*b'\x00'
     TORQUE_CONTROL_BASE: bytes = b'\xA1'
     MOTOR_OFF_BASE: bytes = b'\x80' + 7*b'\x00'
-    current_torque: int = 0
+    K_P: float = 0
+    K_D: float = 0
 
-    def _generate_torque_frame(self, val: int):
-        if val < -2000 or val > 2000:
-            raise ValueError('Torque should be in [-2000; 2000] range')
+    def __init__(self, dev_id: int, bus: CAN_Bus, log: bool = False):
+        super().__init__(dev_id, bus)
+        self.status = CAN_MotorStatus(log=log)
+
+    def _generate_torque_frame(self, val: int, bound = 200):
+        if bound > 2000:
+            raise ValueError('Torque must be in range [-2000; 2000]')
+
+        val = bound if val > bound else val
+        val = -bound if val < -bound else val
+        print(f'torque_val: {val}')
         val_b = val.to_bytes(2, 'little', signed=True)
         temp = self.TORQUE_CONTROL_BASE + 3*b'\x00' + val_b + 2*b'\x00'
-        # temp[4] = val_b[0]
-        # temp[5] = val_b[1]
         return temp
     
-    def _send_n_rcv_torque(self, val: int):
-        self.current_torque = val
-        frame = self._generate_torque_frame(val)
+    def _send_n_rcv_torque(self, val: int, bound=200):
+        frame = self._generate_torque_frame(val, bound=bound)
         self.send(frame)
-        self.recv_data()
+        self.recv_status()
     
-    def smooth_off(self):
-        torques = np.linspace(self.current_torque, 0, 5, dtype=np.int16)
-        for t in torques:
+    def PD_control(self, Kp, Kd, q, qdot, bound = 200, v_filter=False):
+        self.send(self.MOTOR_STATUS_BASE)
+        self.recv_status()
+        i = 0
+        while True:
+            if i %4 == 0:
+
+                # sleep(0.05)
+                qr, qdotr = self.status.get_data(v_filter=v_filter)
+                qr = self._calc_degrees(qr)
+                
+                t = Kp*(q - qr) + Kd*(qdot - qdotr)
+                print(f'pos: {qr}, vel: {qdotr}, torque: {t}', end='')
+                self._send_n_rcv_torque(int(t), bound = bound)
+            i += 1
+            i = i//4
+    
+    def smooth_off(self, Kd=1):
+        self.status.write_log()
+        self.send(self.MOTOR_STATUS_BASE)
+        self.recv_status()
+        con = 2**14
+        while self.status.get_data()[1] != 0 and np.abs((self.status.get_data()[0]/con*360)%360) > 3:
+            q, qdotr = self.status.get_data()
+            q = (q/con*360)%360
+            t = 1*(0 - qdotr) + 10*(0 - q)
             self._send_n_rcv_torque(int(t))
-            sleep(0.1)
-        while np.abs(self.current_vel) > 1:
-            self.recv_data()
-            self.get_inst_vel()
+        sleep(1)
+        self.motor_off()
+        sleep(1)
+        
+
+    
+    # def smooth_off(self):
+    #     torques = np.linspace(self.current_torque, 0, 5, dtype=np.int16)
+    #     for t in torques:
+    #         self._send_n_rcv_torque(int(t))
+    #         sleep(0.1)
+    #     while np.abs(self.current_vel) > 1:
+    #         self.recv_data()
+    #         self.get_inst_vel()
     
     def motor_off(self):
         self.send(self.MOTOR_OFF_BASE)
@@ -75,32 +197,11 @@ class CAN_Motor(CAN_Device):
     def _calc_abs_pos(self) -> int:
         return self.hi*self.current_turns + self.current_position
     
-    def recv_data(self, send: bool=True) -> None:
-        if send:
-            self.send(self.MOTOR_STATUS_BASE)
-        self.current_data = self.recive()
-    
-        
-    def _recv_pos(self, threshold_lo: int=2**4, threshold_hi: int = 2**12) -> None:
-        # self.current_time = time()
-        data = self.current_data
-        if data is None:
-            return
-
-        parsed = int.from_bytes(data[6:],byteorder = 'little')
-        if self.current_position is None:
-            self.current_position = parsed
-        elif self.current_position > threshold_hi and parsed <= threshold_lo:
-            self.current_turns += 1
-        elif self.current_position <= threshold_lo and parsed > threshold_hi:
-            self.current_turns -= 1
-        self.current_position = parsed
-        self.current_positions.pop(0)
-        self.current_positions.append(self._calc_degrees(self.current_position))
+    def recv_status(self) -> None:
+        self.status.process_data(self.recive())
 
     def get_position(self, units: Optional[str] = None) -> Union[float, int]:
-        self._recv_pos()
-        pos = self._calc_abs_pos()
+        pos = self.status.get_data()[0]
         if units is None:
             return pos
         elif units is 'rad':
@@ -111,28 +212,22 @@ class CAN_Motor(CAN_Device):
             raise ValueError(f'No such unit {units}')
 
     def get_inst_vel(self) -> Optional[float]:
-        data = self.current_data
-        if data is None:
-            return
+        return self.status.get_data()[1]
 
-        parsed = int.from_bytes(data[4:6],byteorder = 'little', signed=True)
-        self.current_vel = parsed
-        return self.current_vel
+    # def get_inst_vel_calc(self, threshold_lo: int=2**4, threshold_hi: int = 2**12) -> Optional[float]:
+    #     self.send(self.MOTOR_STATUS_BASE)
+    #     data = self.recive()
+    #     dt = time() - self.current_time
+    #     if data is None:
+    #         return
 
-    def get_inst_vel_calc(self, threshold_lo: int=2**4, threshold_hi: int = 2**12) -> Optional[float]:
-        self.send(self.MOTOR_STATUS_BASE)
-        data = self.recive()
-        dt = time() - self.current_time
-        if data is None:
-            return
+    #     parsed = int.from_bytes(data[6:],byteorder = 'little')
+    #     ds = parsed - self.current_position
+    #     return ds/dt
 
-        parsed = int.from_bytes(data[6:],byteorder = 'little')
-        ds = parsed - self.current_position
-        return ds/dt
-
-    def get_inst_vel_filtered(self, threshold_lo: int=2**4, threshold_hi: int = 2**12) -> Optional[float]:
-        self.recv_data()
-        self._recv_pos()
-        return np.mean(self.current_positions)
+    # def get_inst_vel_filtered(self, threshold_lo: int=2**4, threshold_hi: int = 2**12) -> Optional[float]:
+    #     self.recv_data()
+    #     self._recv_pos()
+    #     return np.mean(self.current_positions)
     
     
